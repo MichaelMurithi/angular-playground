a43273bd0e6195374b0c71ac139b8a5d2998d41f
Michael Murithi <mykemitrichev@gmail.com>
2021-05-23 18:07:34 +0200

    Practiced property binding and interpolation

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list-component.html',
+})
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  products: any[] = [
+    {
+      productId: 1,
+      productName: 'Golden Cat',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2021',
+      description: 'An amazing Golden cat',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
+    },
+    {
+      productId: 2,
+      productName: 'Amazing Goat',
+      productCode: 'AMG-0023',
+      releaseDate: 'March 18, 2021',
+      description: 'An amazing Golden goat',
+      price: 54.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/hammer.png',
+    },
+  ];
+}
