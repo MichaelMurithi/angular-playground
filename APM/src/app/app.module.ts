fba057405a2fdfd9b075dfc8b31cd5c2f0b4c3e9
Michael Murithi <mykemitrichev@gmail.com>
2021-05-23 18:53:49 +0200

    implemented two-way data binding

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
