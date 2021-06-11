1cf214b5ac46c8af9f40383f3db59f81a3ee2ac2
Michael Murithi <mykemitrichev@gmail.com>
2021-06-11 22:05:25 +0200

    learn(StockApp): practiced two-way data binding

--- stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Stock } from '../../model/stock';
 @Component({
   selector: 'app-create-stock',
   templateUrl: './create-stock.component.html',
-  styleUrls: ['./create-stock.component.css']
+  styleUrls: ['./create-stock.component.css'],
 })
 export class CreateStockComponent implements OnInit {
+  public stock!: Stock;
+  public title: string = 'Create Stock Form';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.stock = new Stock('test', '', 0, 0);
   }
+  ngOnInit(): void {}
 
+  getValue(event: Event) {
+    return (event.target as HTMLInputElement).value;
+  }
 }
