9dfde25fa25a64c13b297381f6331d3f0ab4e466
mykemk <mykemitrichev@gmail.com>
2021-07-02 13:43:48 +0200

    feat(components/event): Created events components

--- youtube-live-clone/src/app/app.module.ts
+++ youtube-live-clone/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventsComponent } from './components/events/events.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
