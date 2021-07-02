3cb1b9341f3da61c95c63245182155065d3cf334
mykemk <mykemitrichev@gmail.com>
2021-07-02 13:48:29 +0200

    feat(app/components): Generated components base files

--- youtube-live-clone/src/app/app.module.ts
+++ youtube-live-clone/src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EventsComponent } from './components/events/events.component';
 import { EventComponent } from './components/event/event.component';
+import { CreateEventComponent } from './components/create-event/create-event.component';
+import { EditEventComponent } from './components/edit-event/edit-event.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    EventComponent
+    EventComponent,
+    CreateEventComponent,
+    EditEventComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
