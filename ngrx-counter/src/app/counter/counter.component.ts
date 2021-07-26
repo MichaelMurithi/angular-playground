dda15a9e095b236af674f6b4b452ce0413729ca2
Michael Murithi <mykemitrichev@gmail.com>
2021-07-26 19:22:05 +0200

    feat(counter): Created counter component

---
+++ ngrx-counter/src/app/counter/counter.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+})
+export class CounterComponent implements OnInit {
+  public countValue: number = 0;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
